c71f58ce560bb30c66ce89b2fb03d5855b8f78cf
Shachar Doron <shachar08.doron@gmail.com>
2026-03-14 13:00:15 +0200

    Initial commit

---
+++ Guess-the-number.js
@@ -0,0 +1,37 @@
+let random;
+let attempts;
+
+//get the values to the range
+document.getElementById("startBtn").onclick = function () {
+    let min = Number(document.getElementById("min").value);
+    let max = Number(document.getElementById("max").value);
+
+    random = Math.floor(Math.random() * (max - min + 1)) + min;
+    attempts = max-min; 
+    document.getElementById("output").textContent = "המשחק התחיל! תתחיל לנחש...";
+};
+
+document.getElementById("guessBtn").onclick = function () {
+    let guess = Number(document.getElementById("guess").value);
+    attempts--;
+
+    if (guess === random) {
+        document.getElementById("output").textContent =
+            "המשחק נגמר - הצלחת לנחש! נשארו לך " + attempts + " ניסיונות";
+        return;
+    }
+
+    if (attempts === 0) {
+        document.getElementById("output").textContent =
+            "הפסדת! המספר היה " + random;
+        return;
+    }
+
+    if (guess > random)  {
+        document.getElementById("output").textContent =
+            "גבוה מדי! נשארו " + attempts + " ניסיונות";
+    } else {
+        document.getElementById("output").textContent =
+            "נמוך מדי! נשארו " + attempts + " ניסיונות";
+    }
+};
