ed053df1a7459b3c510c5b067db7618893e8aaca
ShacharDoron123 <shachar08.doron@gmail.com>
2026-05-19 14:26:27 +0300

    part 1

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,2 +1,65 @@
-const imgs = []
-let word;
+import { countries, vegetablesAndFruits } from "./words.js";
+
+let index = 0; //courent stage of the game
+let word; // the selected word pf the game
+let category;
+let guessedLetters = [];
+let isGame = false;
+
+function Game() {
+  index = 0;
+  guessedLetters = [];
+  isGame = true;
+  document.getElementById("word").textContent = "";
+  category = document.getElementById("selectedCategory").value;
+  word = selecteWord(category);
+  display();
+}
+
+function selecteWord(category) {
+  if (category == "countries") {
+    let random = Math.floor(Math.random() * countries.length);
+    word = countries[random];
+  }
+  if (category == "vegetablesAndFruits") {
+    let random = Math.floor(Math.random() * vegetablesAndFruits.length);
+    word = vegetablesAndFruits[random];
+  }
+  return word;
+}
+
+function check() {
+  if (isGame) {
+    let input = document.getElementById("input").value;
+    if (input.length == 1) {
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] == input) guessedLetters[i] = true;
+      }
+      display();
+    } else if (input.length == word.length) {
+      for (let i = 0; i < word.length; i++) {
+        if (input[i] != word[i]) return;
+      }
+      isGame = false;
+      document.getElementById("word").textContent =
+        "you won, the word was " + word;
+    }
+  }
+  document.getElementById("input").value = "";
+  return;
+}
+
+function display() {
+  let temp = "";
+
+  for (let i = 0; i < word.length; i++) {
+    if (guessedLetters[i] == true) 
+        temp += " "+word[i];
+    else if (word[i] === " ")
+         temp += "  ";
+    else 
+        temp += "_ ";
+  }
+
+  document.getElementById("word").textContent = temp;
+}
---
+++ hangman/words.js
@@ -0,0 +1,2 @@
+export const countries = []
+export const vegetablesAndFruits = []
